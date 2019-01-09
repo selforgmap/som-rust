4d1803fe31097ddcc87f68a0812ee1d373da02d8
Sumedhe Dissanayake <sumedhedissanayake@gmail.com>
2019-01-09 12:31:32 +0530

    Create som struct

--- src/bin.rs
+++ src/bin.rs
@@ -1,6 +1,19 @@
 extern crate som;
-use som::test;
 
+use som::*;
+
+// Main function
 fn main() {
-    test();
+    let som = SOM {
+        size                  : (10, 10),
+        dimension             : 3,
+        grid_type             : GridType::Rectangular,
+        learning_rate         : 0.1,
+        learning_rate_function: LearningRateFunction::Constant,
+        initializing_method   : InitializingMethod::Random,
+        neighborhood_function : NeighborhoodFunction::Bubble,
+        iterations            : 100
+    };
+
+    println!("{}", som.iterations); // Test
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
-pub fn test() {
-  println!("Hello");
+mod enums;
+
+pub use crate::enums::*;
+
+// SOM Struct
+pub struct SOM {
+  pub size                  : (i32, i32),
+  pub dimension             : i32,
+  pub grid_type             : GridType,
+  pub learning_rate         : f32,
+  pub learning_rate_function: LearningRateFunction,
+  pub initializing_method   : InitializingMethod,
+  pub neighborhood_function : NeighborhoodFunction,
+  pub iterations            : i32
+}
+
+impl SOM {
+  pub fn test() {
+    println!("hello")
+  }
 }
